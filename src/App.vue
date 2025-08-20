f67aab7ab4b66ea4295c438c27b666dbb71f57db
David Marr <david.marr@contextlabs.com>
2025-08-20 13:08:18 -0400

    feat: Add example code

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue';
 import Squircle from './components/Squircle.vue';
 
-const radius = ref(100);
+const radius = ref(56);
 const roundness = ref(0.2);
 const width = ref(300);
 const height = ref(200);
@@ -13,15 +13,27 @@ const height = ref(200);
   <a class="github" href="https://github.com/marr/vue-squircle">
     <img src="/github.svg" alt="Vue Squircle Github Repository "/>
   </a>
-  <p>A handy, free way to display pleasantly rounded corners in your web app.</p>
-  <h3>Default values</h3>
-  <Squircle :width="+width" :height="+height">
-    <img :src="`//picsum.photos/${width}/${height}`" />
-  </Squircle>
-  <h3>Customized values</h3>
-  <Squircle :width="+width" :height="+height" :radius="+radius" :roundness="+roundness">
-    <img :src="`//picsum.photos/${width}/${height}`" />
-  </Squircle>
+  <p>Pleasantly rounded corners in your web app.</p>
+  <pre class="example">
+  <code>
+    &lt;Squircle :width :height&gt;
+      &lt;img :src="`//picsum.photos/${width}/${height}`" /&gt;
+    &lt;/Squircle&gt;
+  </code></pre>
+  <div class="hero">
+    <div>
+      <h3>Default values</h3>
+      <Squircle :width="+width" :height="+height">
+        <img :src="`//picsum.photos/${width}/${height}`" />
+      </Squircle>
+    </div>
+    <div>
+      <h3>Customized values</h3>
+      <Squircle :width="+width" :height="+height" :radius="+radius" :roundness="+roundness">
+        <img :src="`//picsum.photos/${width}/${height}`" />
+      </Squircle>
+    </div>
+  </div>
   <div class="controls">
     <label>
       <span>Radius</span>
@@ -48,6 +60,26 @@ const height = ref(200);
   position: fixed;
   top: 16px;
   left: 16px;
+  @media (prefers-color-scheme: dark) {
+    filter: invert(1);
+  }
+}
+.example {
+  background-color: #f9f9f903;
+  border: 1px solid #888d;
+  border-radius: 6px;
+  text-align: left;
+}
+.hero {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  margin-bottom: 20px;
+  @media (min-width: 600px) {
+    flex-direction: row;
+    gap: 40px;
+  }
 }
 .controls {
   border-bottom: 1px solid #888;
